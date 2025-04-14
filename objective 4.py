8cb1c17ddae171da699c5eec08bceee463322111
Saibhargav2005 <153920418+Saibhargav2005@users.noreply.github.com>
2025-04-14 16:32:49 +0530

    Create objective 4.py

---
+++ objective 4.py
@@ -0,0 +1,9 @@
+# Objective 4: Number of Programs Offered Per Year
+data.groupby('Year').size().plot(
+    kind='line', marker='o', color='red', figsize=(10, 6), title="Number of Programs Offered Each Year"
+)
+plt.xlabel("Year")
+plt.ylabel("Number of Programs")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
